b85e9053762b0df0f362bcb13e35280c991e1be4
lucasdegruijter <lucasdegruijter@gmail.com>
2024-03-28 15:40:31 +0100

    Added dashboard quasar JS project

---
+++ RailTestingDashboard/src/layouts/OldMainLayout.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue';
+
+defineOptions({
+  name: 'MainLayout'
+});
+
+const linksList: EssentialLinkProps[] = [
+  {
+    title: 'Dashboard',
+    caption: 'main interface',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Historische gegevens',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Logs',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Instellingen',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  }
+];
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer () {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
+
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in linksList"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
---
+++ RailTestingDashboard/src/pages/DashboardPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+    <q-item-label>test</q-item-label>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  // name: 'PageName'
+})
+</script>
