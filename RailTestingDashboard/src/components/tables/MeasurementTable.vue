7a0b7f73fb6ed76774f12d6faba0344869147ba0
lucasdegruijter <lucasdegruijter@gmail.com>
2024-06-05 11:01:03 +0200

    Changed dahs formatting

--- RailTestingDashboard/src/components/tables/MeasurementTable.vue
+++ RailTestingDashboard/src/components/tables/MeasurementTable.vue
@@ -14,7 +14,8 @@ export default defineComponent({
     }
   },
   methods: {
-    formatValue(value: string | number): string {
+    formatValue(value: string | number | undefined): string {
+      if (value === undefined || value === null) return '-';
       const num = parseFloat(value as string);
       if (isNaN(num)) return value.toString();
       return num.toFixed(2);
@@ -30,81 +31,75 @@ export default defineComponent({
       title="Meetgegevens"
       :rows="measurements"
       :columns="[
-        { name: 'Hole_1', align: 'left', label: 'Hole 1', field: 'Hole_1', sortable: true },
-        { name: 'Hole_2', align: 'left', label: 'Hole 2', field: 'Hole_2', sortable: true },
-        { name: 'Hole_3', align: 'left', label: 'Hole 3', field: 'Hole_3', sortable: true },
-        { name: 'Hole_4', align: 'left', label: 'Hole 4', field: 'Hole_4', sortable: true },
-        { name: 'Hole_5', align: 'left', label: 'Hole 5', field: 'Hole_5', sortable: true },
-        { name: 'Hole_6', align: 'left', label: 'Hole 6', field: 'Hole_6', sortable: true },
-        { name: 'Hole_7', align: 'left', label: 'Hole 7', field: 'Hole_7', sortable: true },
-        { name: 'Hole_8', align: 'left', label: 'Hole 8', field: 'Hole_8', sortable: true },
-        { name: 'Hole_9', align: 'left', label: 'Hole 9', field: 'Hole_9', sortable: true },
-        { name: 'Hole_10', align: 'left', label: 'Hole 10', field: 'Hole_10', sortable: true },
-        { name: 'Hole_11', align: 'left', label: 'Hole 11', field: 'Hole_11', sortable: true },
-        { name: 'Hole_12', align: 'left', label: 'Hole 12', field: 'Hole_12', sortable: true }
+        { name: 'Steek_1', align: 'left', label: 'Steek 1', field: 'Steek_1', sortable: true },
+        { name: 'Steek_2', align: 'left', label: 'Steek 2', field: 'Steek_2', sortable: true },
+        { name: 'Steek_3', align: 'left', label: 'Steek 3', field: 'Steek_3', sortable: true },
+        { name: 'Steek_4', align: 'left', label: 'Steek 4', field: 'Steek_4', sortable: true },
+        { name: 'Steek_5', align: 'left', label: 'Steek 5', field: 'Steek_5', sortable: true },
+        { name: 'Steek_6', align: 'left', label: 'Steek 6', field: 'Steek_6', sortable: true },
+        { name: 'Steek_7', align: 'left', label: 'Steek 7', field: 'Steek_7', sortable: true },
+        { name: 'Steek_8', align: 'left', label: 'Steek 8', field: 'Steek_8', sortable: true },
+        { name: 'Steek_9', align: 'left', label: 'Steek 9', field: 'Steek_9', sortable: true },
+        { name: 'Steek_10', align: 'left', label: 'Steek 10', field: 'Steek_10', sortable: true },
+        { name: 'Steek_11', align: 'left', label: 'Steek 11', field: 'Steek_11', sortable: true }
       ]"
-      row-key="Hole_1"
+      row-key="Steek_1"
     >
       <template v-slot:body="props">
         <q-tr :props="props">
-          <q-td key="Hole_1" :props="props">
+          <q-td key="Steek_1" :props="props">
             <q-badge color="green">
-              {{ formatValue(props.row.Hole_1) }}
+              {{ formatValue(props.row.Steek_1) }}
             </q-badge>
           </q-td>
-          <q-td key="Hole_2" :props="props">
+          <q-td key="Steek_2" :props="props">
             <q-badge color="green">
-              {{ formatValue(props.row.Hole_2) }}
+              {{ formatValue(props.row.Steek_2) }}
             </q-badge>
           </q-td>
-          <q-td key="Hole_3" :props="props">
+          <q-td key="Steek_3" :props="props">
             <q-badge color="green">
-              {{ formatValue(props.row.Hole_3) }}
+              {{ formatValue(props.row.Steek_3) }}
             </q-badge>
           </q-td>
-          <q-td key="Hole_4" :props="props">
+          <q-td key="Steek_4" :props="props">
             <q-badge color="green">
-              {{ formatValue(props.row.Hole_4) }}
+              {{ formatValue(props.row.Steek_4) }}
             </q-badge>
           </q-td>
-          <q-td key="Hole_5" :props="props">
+          <q-td key="Steek_5" :props="props">
             <q-badge color="green">
-              {{ formatValue(props.row.Hole_5) }}
+              {{ formatValue(props.row.Steek_5) }}
             </q-badge>
           </q-td>
-          <q-td key="Hole_6" :props="props">
+          <q-td key="Steek_6" :props="props">
             <q-badge color="green">
-              {{ formatValue(props.row.Hole_6) }}
+              {{ formatValue(props.row.Steek_6) }}
             </q-badge>
           </q-td>
-          <q-td key="Hole_7" :props="props">
+          <q-td key="Steek_7" :props="props">
             <q-badge color="green">
-              {{ formatValue(props.row.Hole_7) }}
+              {{ formatValue(props.row.Steek_7) }}
             </q-badge>
           </q-td>
-          <q-td key="Hole_8" :props="props">
+          <q-td key="Steek_8" :props="props">
             <q-badge color="green">
-              {{ formatValue(props.row.Hole_8) }}
+              {{ formatValue(props.row.Steek_8) }}
             </q-badge>
           </q-td>
-          <q-td key="Hole_9" :props="props">
+          <q-td key="Steek_9" :props="props">
             <q-badge color="green">
-              {{ formatValue(props.row.Hole_9) }}
+              {{ formatValue(props.row.Steek_9) }}
             </q-badge>
           </q-td>
-          <q-td key="Hole_10" :props="props">
+          <q-td key="Steek_10" :props="props">
             <q-badge color="green">
-              {{ formatValue(props.row.Hole_10) }}
+              {{ formatValue(props.row.Steek_10) }}
             </q-badge>
           </q-td>
-          <q-td key="Hole_11" :props="props">
+          <q-td key="Steek_11" :props="props">
             <q-badge color="green">
-              {{ formatValue(props.row.Hole_11) }}
-            </q-badge>
-          </q-td>
-          <q-td key="Hole_12" :props="props">
-            <q-badge color="green">
-              {{ formatValue(props.row.Hole_12) }}
+              {{ formatValue(props.row.Steek_11) }}
             </q-badge>
           </q-td>
         </q-tr>
